703f0a332aaaad0bca5996f8e5a2a4df8ed282d9
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-11-25 09:38:59 -0300

    instance express

---
+++ Introduçao ao Express/express.js
@@ -0,0 +1,7 @@
+var express = require("express")
+
+// var express = funçao construtora do express // var express = construct function of express //
+
+var app = express();
+
+// var app = uma nova instancia do express gerada a partir da funçao construtora // var app = uma nova instancia do express gerada a partir da funçao construtora //
