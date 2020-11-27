50813cc96267affdc58636cbc59e3fc4d6ae0d05
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-11-27 16:36:42 -0300

    create table: Users

--- mysql_connection/models.js
+++ mysql_connection/models.js
@@ -21,8 +21,31 @@ const postagem = sequelize.define('postagems', {
 })
 
 // Sincroniza com o banco, fazendo assim ele criar a tabela // Synchronizes with the database, making it create the table //
-
+/*
 postagem.sync({
    // para garantir que a tabela será criada // to make sure the table will be created //
     force: true
 })
+*/
+
+const user = sequelize.define("Users",{
+
+    name: {
+        type: Sequelize.STRING
+    },
+
+    surname: {
+        type: Sequelize.STRING
+    },
+
+    age: {
+        type: Sequelize.INTEGER
+    },
+
+    email: {
+        type: Sequelize.STRING 
+    }
+})
+
+//user.sync({force: true})
+
