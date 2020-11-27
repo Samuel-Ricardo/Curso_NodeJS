d274597f57ede02024c104e6bae37251f5d459e2
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-11-27 16:13:23 -0300

    refactor

--- mysql_connection/test.js
+++ mysql_connection/test.js
@@ -1,12 +1,19 @@
 const Sequelize = require('sequelize')
 
+// connect whit database
 const sequelize = new Sequelize("siscad","root","",{
     host: "localhost",
+    port: "3308",
     dialect: "mysql"  
 })
 
+// verify connection
 sequelize.authenticate().then(function(){
 
-}).catch(() => {
+    console.log("Conectado com sucesso")
+
+}).catch((error) => {
     
+    console.log('Erro ao Conectar: '+ error)
+
 })
