05cafb82d3bffc546e23993a8b8dc6321f0c8194
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-12-03 23:26:24 -0300

    create table

---
+++ Projects/Postagens/Model/Post.js
@@ -0,0 +1,21 @@
+const dataBase = require('../DAO/ConnectionFactory')
+
+/*
+
+// Deve ser executado apenas uma vez, para criar a tabela // Must be executed only once, to create the table // 
+
+const Post = dataBase.sequelize.define('posts', {
+
+    title: {
+        type: dataBase.Sequelize.STRING
+    },
+
+    conteudo: {
+        type: dataBase.Sequelize.TEXT 
+    }
+})
+
+Post.sync({force: true})
+
+*/
+
